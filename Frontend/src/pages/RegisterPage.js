6c42f6cca103135a13f962c7722e2cc131ec37f8
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-23 22:58:40 +0530

    Update RegisterPage.js

--- Frontend/src/pages/RegisterPage.js
+++ Frontend/src/pages/RegisterPage.js
@@ -1,17 +1,16 @@
-
-// src/pages/LoginPage.js
+// src/pages/RegisterPage.js
 import React, { useState } from "react";
 import { Form, Button, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const { login } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     const res = await fetch("https://coding-journal-hqbn.onrender.com/api/users/register", {
       method: "POST",
@@ -31,22 +30,31 @@ const LoginPage = () => {
   return (
     <Card className="form-wrapper">
       <Card.Body>
-        <h2>Login</h2>
-        <Form onSubmit={handleLogin}>
+        <h2>Register</h2>
+        <Form onSubmit={handleRegister}>
           <Form.Group className="mb-3">
             <Form.Label>Email</Form.Label>
-            <Form.Control value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </Form.Group>
-          <Button type="submit">Login</Button>
+          <Button type="submit">Register</Button>
         </Form>
       </Card.Body>
     </Card>
   );
 };
 
-export default LoginPage;
-
+export default RegisterPage;
