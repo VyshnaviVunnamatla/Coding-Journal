8208b20eb851b256a0f280010e5a06325595011d
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-23 23:51:27 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,16 +1,25 @@
+// authMiddleware.js
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const protect = async (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: "Not authorized" });
+  let token;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password");
-    next();
-  } catch (err) {
-    res.status(401).json({ message: "Token failed" });
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (err) {
+      res.status(401).json({ message: "Invalid token" });
+    }
+  } else {
+    res.status(401).json({ message: "No token provided" });
   }
 };
+
 export default protect;
