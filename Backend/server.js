12c3fafa17df87f15f129eee7491c0884803a9fd
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-23 18:20:53 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db");
-const problemRoutes = require("./routes/problemRoutes");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import problemRoutes from "./routes/problemRoutes.js";
 
 dotenv.config();
 connectDB();
