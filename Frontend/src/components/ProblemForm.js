27abd1776fcab59ba5b0823eedecb5d1ad1c4426
VyshnaviVunnamatla <165186336+VyshnaviVunnamatla@users.noreply.github.com>
2025-07-24 23:04:30 +0530

    Update ProblemForm.js

--- Frontend/src/components/ProblemForm.js
+++ Frontend/src/components/ProblemForm.js
@@ -79,7 +79,7 @@ function ProblemForm() {
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Output</Form.Label>
-              <Form.Control as="textarea" value={(e) => setOutputText(e.target.value)} rows={4} readOnly />
+              <Form.Control as="textarea" value={outputtext} onChange={(e) => setOutputText(e.target.value)} rows={4} readOnly />
             </Form.group>
             <Button type="submit">{id ? "Update" : "Create"}</Button>
         </Card.Body>
